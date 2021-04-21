5a7d2f4c6bd7e2e22973ddf9ae9f8c9e1f457935
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-04-21 09:21:27 +0200

    push

---
+++ main.js
@@ -0,0 +1,27 @@
+let day = prompt("See what you have to do this week?");
+
+switch (day) {
+    case 'lundi':
+    alert('Salle de sport à 8h00 - cours de curling sur gazon à 10h30 - dejeuner en famille - chilling');
+    break;
+    case 'mardi':
+    alert('Code à 8h10 - promener les chiens à 11h - vibin');
+    break;
+    case 'mercredi':
+    alert('tu ne fais rien ajd, la chance');
+    break;
+    case 'jeudi':
+    alert('regarder les episodes de "the last dance"');
+    break;
+    case 'vendredi':
+    alert('aller voir les copains et voila. Une fille veut te voir aussi');
+    break;
+    case 'samedi':
+    alert('vélo et sex');
+    break;
+    case 'dimanche':
+    alert('profiter du soleil et crier liberter dans le bois de la cambre');
+    break;
+    default:
+    alert(`si tu n'as rien à faire, va faire du skate.`);
+}
